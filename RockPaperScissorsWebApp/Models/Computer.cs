04802dc25fa17d6fb959e3229e7cdfa0cc75a493
Ajeet <ajeet_athwal@hotmail.co.uk>
2021-04-11 19:27:50 +0100

    Add Computer class (inherits from User)

---
+++ RockPaperScissorsWebApp/Models/Computer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RockPaperScissorsWebApp.Models
+{
+    public class Computer : User
+    {
+        public Computer(RandomRPSChoice randomRPSChoice) : base()
+        {
+            this.randomRPSChoice = randomRPSChoice;
+        }
+
+        public override RPSChoice getRPSChoice()
+        {
+            return randomRPSChoice.Get();
+        }
+
+        RandomRPSChoice randomRPSChoice;
+    }
+}
---
+++ RockPaperScissorsWebApp/Models/RandomRPSChoice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RockPaperScissorsWebApp.Models
+{
+    public class RandomRPSChoice
+    {
+        public RandomRPSChoice()
+        {
+            r = new Random();
+        }
+
+        public RPSChoice Get()
+        {
+            return (RPSChoice)r.Next(0, Enum.GetValues(typeof(RPSChoice)).Length);
+        }
+
+        readonly Random r;
+    }
+}
