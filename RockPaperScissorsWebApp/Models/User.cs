96f871e9c347355460efc834c21527c703e3408a
Ajeet <ajeet_athwal@hotmail.co.uk>
2021-04-11 19:25:19 +0100

    add abstract User Class

---
+++ RockPaperScissorsWebApp/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RockPaperScissorsWebApp.Models
+{
+    public abstract class User
+    {
+        public User()
+        {
+            wins = 0;
+        }
+        public void addWin()
+        {
+            wins++;
+        }
+
+        public abstract RPSChoice getRPSChoice();
+        private int wins;
+
+        public int Wins { get => wins; }
+    }
+}
