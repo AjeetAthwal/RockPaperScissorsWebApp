72b27b7999135eb2e1c81418be4a5da49146ea0e
Ajeet <ajeet_athwal@hotmail.co.uk>
2021-04-11 23:13:00 +0100

    implement Player class

---
+++ RockPaperScissorsWebApp/Models/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RockPaperScissorsWebApp.Models
+{
+    public class Player : User
+    {
+        public override RPSChoice getRPSChoice(RPSChoice rPSChoice)
+        {
+            return rPSChoice;
+        }
+    }
+}
