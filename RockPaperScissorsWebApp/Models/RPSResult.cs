f7ce1c9fe5a04ebff453041f2ae457fae035443d
Ajeet <ajeet_athwal@hotmail.co.uk>
2021-04-11 19:15:21 +0100

    add basic RPS Game logic

---
+++ RockPaperScissorsWebApp/Models/RPSResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RockPaperScissorsWebApp.Models
+{
+    public enum RPSResult
+    {
+        WIN,
+        DRAW,
+        LOSS
+    }
+    public static class RPSResultExtension
+    {
+        public static RPSResult Beats(RPSChoice choice1, RPSChoice choice2)
+        {
+            if (choice1 == choice2) return RPSResult.DRAW;
+            if (((int)choice1 + 1) % Enum.GetValues(typeof(RPSChoice)).Length == (int)choice2)
+            {
+                return RPSResult.LOSS;
+            }
+            return RPSResult.WIN;
+        }
+    }
+}
