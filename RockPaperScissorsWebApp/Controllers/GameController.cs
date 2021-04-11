d33bb25b26f12d51b83c0b5235f0ab9519e1e42c
Ajeet <ajeet_athwal@hotmail.co.uk>
2021-04-11 23:23:22 +0100

    Add Controller and Data for player game

--- RockPaperScissorsWebApp/Controllers/GameController.cs
+++ RockPaperScissorsWebApp/Controllers/GameController.cs
@@ -10,10 +10,12 @@ namespace RockPaperScissorsWebApp.Controllers
     public class GameController : Controller
     {
         private readonly Game computerGame;
+        private readonly Game playerGame;
 
-        public GameController(ComputerGameData computerGame)
+        public GameController(ComputerGameData computerGame, PlayerGameData playerGame)
         {
             this.computerGame = computerGame.game;
+            this.playerGame = playerGame.game;
         }
 
         // GET: Game
@@ -24,17 +26,31 @@ namespace RockPaperScissorsWebApp.Controllers
             return View(model);
         }
 
-        [HttpGet]
-        public ActionResult PlayerVsComputer()
-        {
-            return View();
-        }
-
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult ComputerVsComputer(FormCollection form)
         {
             if (form["submit"] == "Play Round") computerGame.playRound((RPSChoice)0, (RPSChoice)0);
+            else if (form["submit"] == "Reset Scores") computerGame.resetScores();
+
+            var model = computerGame;
+            return RedirectToAction("ComputerVsComputer", model);
+        }
+
+        [HttpGet]
+        public ActionResult PlayerVsComputer()
+        {
+            var model = playerGame;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult PlayerVsComputer(FormCollection form)
+        {
+            if (form["submit"] == "Rock") computerGame.playRound(RPSChoice.ROCK, (RPSChoice)0);
+            else if (form["submit"] == "Scissors") computerGame.playRound(RPSChoice.SCISSORS, (RPSChoice)0);
+            else if (form["submit"] == "Paper") computerGame.playRound(RPSChoice.PAPER, (RPSChoice)0);
             else if (form["submit"] == "Reset Scores") computerGame.resetScores();
 
             var model = computerGame;
--- RockPaperScissorsWebApp/Models/GameData.cs
+++ RockPaperScissorsWebApp/Models/GameData.cs
@@ -7,10 +7,18 @@ namespace RockPaperScissorsWebApp.Models
 {
     public class ComputerGameData
     {
-
         public ComputerGameData()
         {
             game = new Game(GameState.COMPUTERVSCOMPUTER);
+        }
+        public Game game;
+    }
+
+    public class PlayerGameData
+    {
+        public PlayerGameData()
+        {
+            game = new Game(GameState.PLAYERVSCOMPUTER);
         }
         public Game game;
     }
