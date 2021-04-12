2bdd83f6f7c8ea527884e2fd19903ae312cd14cc
Ajeet <ajeet_athwal@hotmail.co.uk>
2021-04-12 19:15:40 +0100

    refactor rest of Scoreboard UnitTests

--- RockPaperScissorsUnitTests/ScoreboardTests.cs
+++ RockPaperScissorsUnitTests/ScoreboardTests.cs
@@ -15,6 +15,13 @@ namespace RockPaperScissorsUnitTests
             return new Scoreboard(player1, player2);
         }
 
+        private Scoreboard CreateNonEmptyScoreboard()
+        {
+            Scoreboard scoreboard = CreateEmptyScoreboard();
+            scoreboard.update(RPSP1P2Result.DRAW);
+            return scoreboard;
+        }
+
         [TestMethod]
         public void update_P1WIN_ReturnsScoreboardWithOneWinForPlayerOne()
         {
@@ -80,100 +87,243 @@ namespace RockPaperScissorsUnitTests
         }
 
         [TestMethod]
-        public void AddingP2WinWorks()
+        public void update_P2WIN_ReturnsScoreboardWithNoWinForPlayerOne()
         {
             // Arrange
-            Scoreboard scoreboard = CreateEmptyScoreboard();
+            Scoreboard actualScoreboard = CreateEmptyScoreboard();
             RPSP1P2Result newRPSresult = RPSP1P2Result.P2WIN;
             int expectedP1Wins = 0;
-            int expectedP2Wins = 1;
-            int expectedDraws = 0;
-            int expectedgamesPlayed = 1;
 
             // Act
-            scoreboard.update(newRPSresult);
+            actualScoreboard.update(newRPSresult);
+            int actualP1Wins = actualScoreboard.Player1Wins;
 
             // Assert
-            Assert.AreEqual(expectedP1Wins, scoreboard.Player1Wins);
-            Assert.AreEqual(expectedP2Wins, scoreboard.Player2Wins);
-            Assert.AreEqual(expectedDraws, scoreboard.Draws);
-            Assert.AreEqual(expectedgamesPlayed, scoreboard.GamesPlayed);
+            Assert.AreEqual(expectedP1Wins, actualP1Wins);
         }
 
         [TestMethod]
-        public void AddingDrawWorks()
+        public void update_P2WIN_ReturnsScoreboardWithOneWinForPlayerTwo()
         {
             // Arrange
-            Scoreboard scoreboard = CreateEmptyScoreboard();
+            Scoreboard actualScoreboard = CreateEmptyScoreboard();
+            RPSP1P2Result newRPSresult = RPSP1P2Result.P2WIN;
+            int expectedP2Wins = 1;
+
+            // Act
+            actualScoreboard.update(newRPSresult);
+            int actualP2Wins = actualScoreboard.Player2Wins;
+
+            // Assert
+            Assert.AreEqual(expectedP2Wins, actualP2Wins);
+        }
+
+        [TestMethod]
+        public void update_P2WIN_ReturnsScoreboardWithNoDraw()
+        {
+            // Arrange
+            Scoreboard actualScoreboard = CreateEmptyScoreboard();
+            RPSP1P2Result newRPSresult = RPSP1P2Result.P2WIN;
+            int expectedDraws = 0;
+
+            // Act
+            actualScoreboard.update(newRPSresult);
+            int actualDraws = actualScoreboard.Draws;
+
+            // Assert
+            Assert.AreEqual(expectedDraws, actualDraws);
+        }
+
+        [TestMethod]
+        public void update_P2WIN_ReturnsScoreboardWithOneGamePlayed()
+        {
+            // Arrange
+            Scoreboard actualScoreboard = CreateEmptyScoreboard();
+            RPSP1P2Result newRPSresult = RPSP1P2Result.P2WIN;
+            int expectedGamesPlayed = 1;
+
+            // Act
+            actualScoreboard.update(newRPSresult);
+            int actualGamesPlayed = actualScoreboard.GamesPlayed;
+
+            // Assert
+            Assert.AreEqual(expectedGamesPlayed, actualGamesPlayed);
+        }
+
+        [TestMethod]
+        public void update_DRAW_ReturnsScoreboardWithNoWinForPlayerOne()
+        {
+            // Arrange
+            Scoreboard actualScoreboard = CreateEmptyScoreboard();
             RPSP1P2Result newRPSresult = RPSP1P2Result.DRAW;
             int expectedP1Wins = 0;
-            int expectedP2Wins = 0;
-            int expectedDraws = 1;
-            int expectedgamesPlayed = 1;
 
             // Act
-            scoreboard.update(newRPSresult);
+            actualScoreboard.update(newRPSresult);
+            int actualP1Wins = actualScoreboard.Player1Wins;
 
             // Assert
-            Assert.AreEqual(expectedP1Wins, scoreboard.Player1Wins);
-            Assert.AreEqual(expectedP2Wins, scoreboard.Player2Wins);
-            Assert.AreEqual(expectedDraws, scoreboard.Draws);
-            Assert.AreEqual(expectedgamesPlayed, scoreboard.GamesPlayed);
+            Assert.AreEqual(expectedP1Wins, actualP1Wins);
         }
 
         [TestMethod]
-        public void AddingMultipleResultsWorks()
+        public void update_DRAW_ReturnsScoreboardWithNoWinForPlayerTwo()
         {
             // Arrange
-            Scoreboard scoreboard = CreateEmptyScoreboard();
-            RPSP1P2Result newRPSresult1 = RPSP1P2Result.DRAW;
-            RPSP1P2Result newRPSresult2 = RPSP1P2Result.P1WIN;
-            RPSP1P2Result newRPSresult3 = RPSP1P2Result.P1WIN;
-            RPSP1P2Result newRPSresult4 = RPSP1P2Result.P2WIN;
-            int expectedP1Wins = 2;
-            int expectedP2Wins = 1;
-            int expectedDraws = 1;
-            int expectedgamesPlayed = 4;
+            Scoreboard actualScoreboard = CreateEmptyScoreboard();
+            RPSP1P2Result newRPSresult = RPSP1P2Result.DRAW;
+            int expectedP2Wins = 0;
 
             // Act
-            scoreboard.update(newRPSresult1);
-            scoreboard.update(newRPSresult2);
-            scoreboard.update(newRPSresult3);
-            scoreboard.update(newRPSresult4);
+            actualScoreboard.update(newRPSresult);
+            int actualP2Wins = actualScoreboard.Player2Wins;
 
             // Assert
-            Assert.AreEqual(expectedP1Wins, scoreboard.Player1Wins);
-            Assert.AreEqual(expectedP2Wins, scoreboard.Player2Wins);
-            Assert.AreEqual(expectedDraws, scoreboard.Draws);
-            Assert.AreEqual(expectedgamesPlayed, scoreboard.GamesPlayed);
+            Assert.AreEqual(expectedP2Wins, actualP2Wins);
         }
 
         [TestMethod]
-        public void ResetScoresWorks()
+        public void update_DRAW_ReturnsScoreboardWithOneDraw()
+        {
+            // Arrange
+            Scoreboard actualScoreboard = CreateEmptyScoreboard();
+            RPSP1P2Result newRPSresult = RPSP1P2Result.DRAW;
+            int expectedDraws = 1;
+
+            // Act
+            actualScoreboard.update(newRPSresult);
+            int actualDraws = actualScoreboard.Draws;
+
+            // Assert
+            Assert.AreEqual(expectedDraws, actualDraws);
+        }
+
+        [TestMethod]
+        public void update_DRAW_ReturnsScoreboardWithOneGamePlayed()
+        {
+            // Arrange
+            Scoreboard actualScoreboard = CreateEmptyScoreboard();
+            RPSP1P2Result newRPSresult = RPSP1P2Result.DRAW;
+            int expectedGamesPlayed = 1;
+
+            // Act
+            actualScoreboard.update(newRPSresult);
+            int actualGamesPlayed = actualScoreboard.GamesPlayed;
+
+            // Assert
+            Assert.AreEqual(expectedGamesPlayed, actualGamesPlayed);
+        }
+
+        [TestMethod]
+        public void reset_EmptyScoreboard_ReturnsScoreboardWithNoGamePlayed()
         {
             // Arrange
             Scoreboard scoreboard = CreateEmptyScoreboard();
-            RPSP1P2Result newRPSresult1 = RPSP1P2Result.DRAW;
-            RPSP1P2Result newRPSresult2 = RPSP1P2Result.P1WIN;
-            RPSP1P2Result newRPSresult3 = RPSP1P2Result.P1WIN;
-            RPSP1P2Result newRPSresult4 = RPSP1P2Result.P2WIN;
-            int expectedP1Wins = 0;
-            int expectedP2Wins = 0;
+            int expectedGamesPlayed = 0;
+
+            // Act
+            scoreboard.reset();
+
+            // Assert
+            Assert.AreEqual(expectedGamesPlayed, scoreboard.GamesPlayed);
+        }
+
+        [TestMethod]
+        public void reset_EmptyScoreboard_ReturnsScoreboardWithNoDraw()
+        {
+            // Arrange
+            Scoreboard scoreboard = CreateEmptyScoreboard();
             int expectedDraws = 0;
-            int expectedgamesPlayed = 0;
 
             // Act
-            scoreboard.update(newRPSresult1);
-            scoreboard.update(newRPSresult2);
-            scoreboard.update(newRPSresult3);
-            scoreboard.update(newRPSresult4);
+            scoreboard.reset();
+
+            // Assert
+            Assert.AreEqual(expectedDraws, scoreboard.Draws);
+        }
+
+        [TestMethod]
+        public void reset_EmptyScoreboard_ReturnsScoreboardWithNoWinForPlayerOne()
+        {
+            // Arrange
+            Scoreboard scoreboard = CreateEmptyScoreboard();
+            int expectedP1Wins = 0;
+
+            // Act
             scoreboard.reset();
 
             // Assert
             Assert.AreEqual(expectedP1Wins, scoreboard.Player1Wins);
+        }
+
+        [TestMethod]
+        public void reset_EmptyScoreboard_ReturnsScoreboardWithNoWinForPlayerTwo()
+        {
+            // Arrange
+            Scoreboard scoreboard = CreateEmptyScoreboard();
+            int expectedP2Wins = 0;
+
+            // Act
+            scoreboard.reset();
+
+            // Assert
             Assert.AreEqual(expectedP2Wins, scoreboard.Player2Wins);
+        }
+
+        [TestMethod]
+        public void reset_NonEmptyScoreboard_ReturnsScoreboardWithNoGamePlayed()
+        {
+            // Arrange
+            Scoreboard scoreboard = CreateNonEmptyScoreboard();
+            int expectedGamesPlayed = 0;
+
+            // Act
+            scoreboard.reset();
+
+            // Assert
+            Assert.AreEqual(expectedGamesPlayed, scoreboard.GamesPlayed);
+        }
+
+        [TestMethod]
+        public void reset_NonEmptyScoreboard_ReturnsScoreboardWithNoDraw()
+        {
+            // Arrange
+            Scoreboard scoreboard = CreateNonEmptyScoreboard();
+            int expectedDraws = 0;
+
+            // Act
+            scoreboard.reset();
+
+            // Assert
             Assert.AreEqual(expectedDraws, scoreboard.Draws);
-            Assert.AreEqual(expectedgamesPlayed, scoreboard.GamesPlayed);
+        }
+
+        [TestMethod]
+        public void reset_NonEmptyScoreboard_ReturnsScoreboardWithNoWinForPlayerOne()
+        {
+            // Arrange
+            Scoreboard scoreboard = CreateNonEmptyScoreboard();
+            int expectedP1Wins = 0;
+
+            // Act
+            scoreboard.reset();
+
+            // Assert
+            Assert.AreEqual(expectedP1Wins, scoreboard.Player1Wins);
+        }
+
+        [TestMethod]
+        public void reset_NonEmptyScoreboard_ReturnsScoreboardWithNoWinForPlayerTwo()
+        {
+            // Arrange
+            Scoreboard scoreboard = CreateNonEmptyScoreboard();
+            int expectedP2Wins = 0;
+
+            // Act
+            scoreboard.reset();
+
+            // Assert
+            Assert.AreEqual(expectedP2Wins, scoreboard.Player2Wins);
         }
     }
 }
