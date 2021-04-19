fb1771f056b2259305ecf19e30136ace01df2375
Ajeet Athwal <ajeet_athwal@hotmail.co.uk>
2021-04-19 21:56:59 +0100

    add fully fleshed out ComputerGameTests

--- RockPaperScissorsUnitTests/ComputerGameTests.cs
+++ RockPaperScissorsUnitTests/ComputerGameTests.cs
@@ -19,7 +19,13 @@ namespace RockPaperScissorsUnitTests
             return seed;
         }
 
-
+        /// <summary>
+        /// A list of 100 random RPSChoices should be returned when a when a 
+        /// game with a set seed invokes PlayRound() 50 times and Player1Choice
+        /// and Player2Choice are recorded. These random choices should match 
+        /// what is returned when RandomRPSChoice invokes Get() 100 times with 
+        /// the same set seed
+        /// </summary>
         [TestMethod]
         public void PlayRound_50Rounds_returnsRandomRPSChoice()
         {
@@ -48,6 +54,52 @@ namespace RockPaperScissorsUnitTests
 
             // Assert
             Assert.IsTrue(expected.SequenceEqual(actual));
+        }
+
+        /// <summary>
+        /// An argument exception should be encountered when PlayRound() is 
+        /// invoked with one argument. It should only work with no arguments. 
+        /// </summary>
+        [DataTestMethod]
+        [DataRow(RPSChoice.PAPER)]
+        [DataRow(RPSChoice.ROCK)]
+        [DataRow(RPSChoice.SCISSORS)]
+        public void PlayRound_1arg_ThrowsArgumentException(RPSChoice input)
+        {
+            // Arrange
+            ComputerGame computerGame = new ComputerGame();
+
+            // Act
+            Action actual = () => computerGame.PlayRound(input);
+
+            // Assert
+            Assert.ThrowsException<ArgumentException>(actual);
+        }
+
+        /// <summary>
+        /// An argument exception should be encountered when PlayRound() is 
+        /// invoked with two arguments. It should only work with no arguments. 
+        /// </summary>
+        [DataTestMethod]
+        [DataRow(RPSChoice.PAPER, RPSChoice.PAPER)]
+        [DataRow(RPSChoice.PAPER, RPSChoice.ROCK)]
+        [DataRow(RPSChoice.PAPER, RPSChoice.SCISSORS)]
+        [DataRow(RPSChoice.ROCK, RPSChoice.PAPER)]
+        [DataRow(RPSChoice.ROCK, RPSChoice.ROCK)]
+        [DataRow(RPSChoice.ROCK, RPSChoice.SCISSORS)]
+        [DataRow(RPSChoice.SCISSORS, RPSChoice.PAPER)]
+        [DataRow(RPSChoice.SCISSORS, RPSChoice.ROCK)]
+        [DataRow(RPSChoice.SCISSORS, RPSChoice.SCISSORS)]
+        public void PlayRound_2args_ThrowsArgumentException(RPSChoice input1, RPSChoice input2)
+        {
+            // Arrange
+            ComputerGame computerGame = new ComputerGame();
+
+            // Act
+            Action actual = () => computerGame.PlayRound(input1, input2);
+
+            // Assert
+            Assert.ThrowsException<ArgumentException>(actual);
         }
     }
 }
