c49750d905ce849695fc338675a408a8412de8f5
Ajeet Athwal <ajeet_athwal@hotmail.co.uk>
2021-04-19 16:18:28 +0100

    Add comments and additional test to ComputerTests

--- RockPaperScissorsUnitTests/ComputerTests.cs
+++ RockPaperScissorsUnitTests/ComputerTests.cs
@@ -9,18 +9,27 @@ namespace RockPaperScissorsUnitTests
     [TestClass]
     public class ComputerTests
     {
+        /// <summary>
+        /// Helper function that generates a computer with no wins. Tested using Wins_ComputerWithNoWins_returnsZeroWins
+        /// </summary>
         private Computer CreateComputerWithNoWins()
         {
             RandomRPSChoice r = new RandomRPSChoice();
             return new Computer(r);
         }
 
-        private Computer CreateComputerWithSetSeed(int seed)
+        /// <summary>
+        /// Helper function that generates a computer with no wins and a set seed. Tested using Wins_ComputerWithNoWinsAndSetSeed_returnsZeroWins
+        /// </summary>
+        private Computer CreateComputerWithNoWinsAndSetSeed(int seed)
         {
             RandomRPSChoice r = new RandomRPSChoice(seed);
             return new Computer(r);
         }
 
+        /// <summary>
+        /// Helper function that generates a random seed
+        /// </summary>
         private static int CreateSeed()
         {
             Random r1 = new Random();
@@ -28,6 +37,9 @@ namespace RockPaperScissorsUnitTests
             return seed;
         }
 
+        /// <summary>
+        /// Helper function that generates a computer with wins. Tested using Wins_ComputerWithWins_returnsMoreThanZeroWins
+        /// </summary>
         private Computer CreateComputerWithWins()
         {
             Computer computer = CreateComputerWithNoWins();
@@ -35,6 +47,9 @@ namespace RockPaperScissorsUnitTests
             return computer;
         }
 
+        /// <summary>
+        /// Zero should be returned when a computer with no wins has its property Wins called
+        /// </summary>
         [TestMethod]
         public void Wins_ComputerWithNoWins_returnsZeroWins()
         {
@@ -49,6 +64,27 @@ namespace RockPaperScissorsUnitTests
             Assert.AreEqual(expected, actual);
         }
 
+        /// <summary>
+        /// Zero should be returned when a player with no wins has its property Wins called
+        /// </summary>
+        [TestMethod]
+        public void Wins_ComputerWithNoWinsAndSetSeed_returnsZeroWins()
+        {
+            // Arrange
+            int seed = CreateSeed();
+            Computer computer = CreateComputerWithNoWinsAndSetSeed(seed);
+            int expected = 0;
+
+            // Act
+            int actual = computer.Wins;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// One should be returned when a computer with no wins invokes AddWin() and its property Wins called
+        /// </summary>
         [TestMethod]
         public void AddWin_ComputerWithNoWins_returnsOneWin()
         {
@@ -64,6 +100,9 @@ namespace RockPaperScissorsUnitTests
             Assert.AreEqual(expected, actual);
         }
 
+        /// <summary>
+        /// A number greater than 0 should be returned when a computer with wins has its property Wins called
+        /// </summary>
         [TestMethod]
         public void Wins_ComputerWithWins_returnsMoreThanZeroWins()
         {
@@ -78,6 +117,9 @@ namespace RockPaperScissorsUnitTests
             Assert.IsTrue(actual > expectedGreaterThan);
         }
 
+        /// <summary>
+        /// 0 should be returned when a computer with no wins invokes ResetWins() and its property Wins called
+        /// </summary>
         [TestMethod]
         public void ResetWins_ComputerWithNoWins_returnsZeroWins()
         {
@@ -93,6 +135,9 @@ namespace RockPaperScissorsUnitTests
             Assert.AreEqual(expected, actual);
         }
 
+        /// <summary>
+        /// 0 should be returned when a computer with wins invokes ResetWins() and its property Wins called
+        /// </summary>
         [TestMethod]
         public void ResetWins_ComputerWithWins_returnsZeroWins()
         {
@@ -108,13 +153,20 @@ namespace RockPaperScissorsUnitTests
             Assert.AreEqual(expected, actual);
         }
 
+        /// <summary>
+        /// A list of 100 random RSPChoices should be returned when a computer 
+        /// with no wins and a set seed invokes GetRPSChoice() 100 times with 
+        /// ROCK as its input. These random choices should match what is 
+        /// returned when RandomRPSChoice invokes Get() 100 times with the same
+        /// set seed
+        /// </summary>
         [TestMethod]
         public void GetRPSChoice_100Rocks_returnsRandomRPSChoice()
         {
             // Arrange
             int NUMBEROFGETS = 100;
             int seed = CreateSeed();
-            Computer computer = CreateComputerWithSetSeed(seed);
+            Computer computer = CreateComputerWithNoWinsAndSetSeed(seed);
             RandomRPSChoice randomRPSChoice = new RandomRPSChoice(seed);
             RPSChoice input = RPSChoice.ROCK;
             List<RPSChoice> expected = new List<RPSChoice>();
@@ -136,13 +188,20 @@ namespace RockPaperScissorsUnitTests
             Assert.IsTrue(expected.SequenceEqual(actual));
         }
 
+        /// <summary>
+        /// A list of 100 random RSPChoices should be returned when a computer 
+        /// with no wins and a set seed invokes GetRPSChoice() 100 times with 
+        /// PAPER as its input. These random choices should match what is 
+        /// returned when RandomRPSChoice invokes Get() 100 times with the same
+        /// set seed
+        /// </summary>
         [TestMethod]
         public void GetRPSChoice_1000Paper_returnsRandomRPSChoice()
         {
             // Arrange
             int NUMBEROFGETS = 100;
             int seed = CreateSeed();
-            Computer computer = CreateComputerWithSetSeed(seed);
+            Computer computer = CreateComputerWithNoWinsAndSetSeed(seed);
             RandomRPSChoice randomRPSChoice = new RandomRPSChoice(seed);
             RPSChoice input = RPSChoice.PAPER;
             List<RPSChoice> expected = new List<RPSChoice>();
@@ -164,13 +223,20 @@ namespace RockPaperScissorsUnitTests
             Assert.IsTrue(expected.SequenceEqual(actual));
         }
 
+        /// <summary>
+        /// A list of 100 random RSPChoices should be returned when a computer 
+        /// with no wins and a set seed invokes GetRPSChoice() 100 times with 
+        /// SCISSORS as its input. These random choices should match what is 
+        /// returned when RandomRPSChoice invokes Get() 100 times with the same
+        /// set seed
+        /// </summary>
         [TestMethod]
         public void GetRPSChoice_1000Scissors_returnsRandomRPSChoice()
         {
             // Arrange
             int NUMBEROFGETS = 100;
             int seed = CreateSeed();
-            Computer computer = CreateComputerWithSetSeed(seed);
+            Computer computer = CreateComputerWithNoWinsAndSetSeed(seed);
             RandomRPSChoice randomRPSChoice = new RandomRPSChoice(seed);
             RPSChoice input = RPSChoice.SCISSORS;
             List<RPSChoice> expected = new List<RPSChoice>();
