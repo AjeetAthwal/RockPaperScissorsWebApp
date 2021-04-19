578c2708f24772ab231c50bee039213bb7b707b1
Ajeet Athwal <ajeet_athwal@hotmail.co.uk>
2021-04-19 19:49:19 +0100

    add Comments and refactor variable names in ScoreboardTests

--- RockPaperScissorsUnitTests/ScoreboardTests.cs
+++ RockPaperScissorsUnitTests/ScoreboardTests.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RockPaperScissorsWebApp.Models;
-using System;
 
 namespace RockPaperScissorsUnitTests
 {
     [TestClass]
     public class ScoreboardTests
     {
+        /// <summary>
+        /// Helper function that generates a Scoreboard with no wins. Tested using GamesPlayed_CreateEmptyScoreboard_ReturnsScoreboardWithNoGamePlayed
+        /// </summary>
         private Scoreboard CreateEmptyScoreboard()
         {
             RandomRPSChoice r = new RandomRPSChoice();
@@ -15,6 +17,9 @@ namespace RockPaperScissorsUnitTests
             return new Scoreboard(player1, player2);
         }
 
+        /// <summary>
+        /// Helper function that generates a Scoreboard with wins. Tested using GamesPlayed_CreateNonEmptyScoreboard_ReturnsScoreboardWithMoreThanNoGamePlayed
+        /// </summary>
         private Scoreboard CreateNonEmptyScoreboard()
         {
             Scoreboard scoreboard = CreateEmptyScoreboard();
@@ -22,334 +27,440 @@ namespace RockPaperScissorsUnitTests
             return scoreboard;
         }
 
+        /// <summary>
+        /// Zero should be returned when a scoreboard with no games played has its property GamesPlayed called
+        /// </summary>
         [TestMethod]
-        public void CreateEmptyScoreboard__ReturnsScoreboardWithNoGamePlayed()
+        public void GamesPlayed_CreateEmptyScoreboard_ReturnsScoreboardWithNoGamePlayed()
         {
             // Arrange
-            int expectedGamesPlayed = 0;
+            int expected = 0;
 
             // Act
-            Scoreboard actualScoreboard = CreateEmptyScoreboard();
+            Scoreboard actual = CreateEmptyScoreboard();
 
             // Assert
-            Assert.AreEqual(expectedGamesPlayed, actualScoreboard.GamesPlayed);
+            Assert.AreEqual(expected, actual.GamesPlayed);
         }
 
+        /// <summary>
+        /// A number greater than 0 should be returned when a non empty scoreboard has its property GamesPlayed called
+        /// </summary>
         [TestMethod]
-        public void CreateNonEmptyScoreboard__ReturnsScoreboardWithMoreThanNoGamePlayed()
+        public void GamesPlayed_CreateNonEmptyScoreboard_ReturnsScoreboardWithMoreThanNoGamePlayed()
         {
             // Arrange
-            int expectedGamesPlayedGreaterThan = 0;
+            int expectedGreaterThan = 0;
 
             // Act
-            Scoreboard actualScoreboard = CreateNonEmptyScoreboard();
+            Scoreboard actual = CreateNonEmptyScoreboard();
 
             // Assert
-            Assert.IsTrue(actualScoreboard.GamesPlayed > expectedGamesPlayedGreaterThan);
+            Assert.IsTrue(actual.GamesPlayed > expectedGreaterThan);
         }
 
+        /// <summary>
+        /// 1 should be returned when an empty Scoreboard is created and 
+        /// Update() is invoked with argument P1WIN andd its property 
+        /// Player1Wins is called
+        /// </summary>
         [TestMethod]
         public void Update_P1WIN_ReturnsScoreboardWithOneWinForPlayerOne()
         {
             // Arrange
             Scoreboard actualScoreboard = CreateEmptyScoreboard();
             RPSP1P2Result newRPSresult = RPSP1P2Result.P1WIN;
-            int expectedP1Wins = 1;
+            int excpected = 1;
 
             // Act
             actualScoreboard.Update(newRPSresult);
-            int actualP1Wins = actualScoreboard.Player1Wins;
+            int actual = actualScoreboard.Player1Wins;
 
             // Assert
-            Assert.AreEqual(expectedP1Wins, actualP1Wins);
+            Assert.AreEqual(excpected, actual);
         }
 
+        /// <summary>
+        /// 0 should be returned when an empty Scoreboard is created and 
+        /// Update() is invoked with argument P1WIN andd its property 
+        /// Player2Wins is called
+        /// </summary>
         [TestMethod]
         public void Update_P1WIN_ReturnsScoreboardWithNoWinForPlayerTwo()
         {
             // Arrange
             Scoreboard actualScoreboard = CreateEmptyScoreboard();
             RPSP1P2Result newRPSresult = RPSP1P2Result.P1WIN;
-            int expectedP2Wins = 0;
+            int expected = 0;
 
             // Act
             actualScoreboard.Update(newRPSresult);
-            int actualP2Wins = actualScoreboard.Player2Wins;
+            int actual = actualScoreboard.Player2Wins;
 
             // Assert
-            Assert.AreEqual(expectedP2Wins, actualP2Wins) ;
+            Assert.AreEqual(expected, actual) ;
         }
 
+        /// <summary>
+        /// 0 should be returned when an empty Scoreboard is created and 
+        /// Update() is invoked with argument P1WIN andd its property 
+        /// Draws is called
+        /// </summary>
         [TestMethod]
         public void Update_P1WIN_ReturnsScoreboardWithNoDraw()
         {
             // Arrange
             Scoreboard actualScoreboard = CreateEmptyScoreboard();
             RPSP1P2Result newRPSresult = RPSP1P2Result.P1WIN;
-            int expectedDraws = 0;
+            int expected = 0;
 
             // Act
             actualScoreboard.Update(newRPSresult);
-            int actualDraws = actualScoreboard.Draws;
+            int actual = actualScoreboard.Draws;
 
             // Assert
-            Assert.AreEqual(expectedDraws, actualDraws);
+            Assert.AreEqual(expected, actual);
         }
 
+        /// <summary>
+        /// 1 should be returned when an empty Scoreboard is created and 
+        /// Update() is invoked with argument P1WIN andd its property 
+        /// GamesPlayed is called
+        /// </summary>
         [TestMethod]
         public void Update_P1WIN_ReturnsScoreboardWithOneGamePlayed()
         {
             // Arrange
             Scoreboard actualScoreboard = CreateEmptyScoreboard();
             RPSP1P2Result newRPSresult = RPSP1P2Result.P1WIN;
-            int expectedGamesPlayed = 1;
+            int expected = 1;
 
             // Act
             actualScoreboard.Update(newRPSresult);
-            int actualGamesPlayed = actualScoreboard.GamesPlayed;
+            int actual = actualScoreboard.GamesPlayed;
 
             // Assert
-            Assert.AreEqual(expectedGamesPlayed, actualGamesPlayed);
+            Assert.AreEqual(expected, actual);
         }
 
+        /// <summary>
+        /// 0 should be returned when an empty Scoreboard is created and 
+        /// Update() is invoked with argument P2WIN andd its property 
+        /// Player1Wins is called
+        /// </summary>
         [TestMethod]
         public void Update_P2WIN_ReturnsScoreboardWithNoWinForPlayerOne()
         {
             // Arrange
             Scoreboard actualScoreboard = CreateEmptyScoreboard();
             RPSP1P2Result newRPSresult = RPSP1P2Result.P2WIN;
-            int expectedP1Wins = 0;
+            int expected = 0;
 
             // Act
             actualScoreboard.Update(newRPSresult);
-            int actualP1Wins = actualScoreboard.Player1Wins;
+            int actual = actualScoreboard.Player1Wins;
 
             // Assert
-            Assert.AreEqual(expectedP1Wins, actualP1Wins);
+            Assert.AreEqual(expected, actual);
         }
 
+        /// <summary>
+        /// 1 should be returned when an empty Scoreboard is created and 
+        /// Update() is invoked with argument P2WIN andd its property 
+        /// Player2Wins is called
+        /// </summary>
         [TestMethod]
         public void Update_P2WIN_ReturnsScoreboardWithOneWinForPlayerTwo()
         {
             // Arrange
             Scoreboard actualScoreboard = CreateEmptyScoreboard();
             RPSP1P2Result newRPSresult = RPSP1P2Result.P2WIN;
-            int expectedP2Wins = 1;
+            int expected = 1;
 
             // Act
             actualScoreboard.Update(newRPSresult);
-            int actualP2Wins = actualScoreboard.Player2Wins;
+            int actual = actualScoreboard.Player2Wins;
 
             // Assert
-            Assert.AreEqual(expectedP2Wins, actualP2Wins);
+            Assert.AreEqual(expected, actual);
         }
 
+        /// <summary>
+        /// 0 should be returned when an empty Scoreboard is created and 
+        /// Update() is invoked with argument P2WIN andd its property 
+        /// Draws is called
+        /// </summary>
         [TestMethod]
         public void Update_P2WIN_ReturnsScoreboardWithNoDraw()
         {
             // Arrange
             Scoreboard actualScoreboard = CreateEmptyScoreboard();
             RPSP1P2Result newRPSresult = RPSP1P2Result.P2WIN;
-            int expectedDraws = 0;
+            int expected = 0;
 
             // Act
             actualScoreboard.Update(newRPSresult);
-            int actualDraws = actualScoreboard.Draws;
+            int actual = actualScoreboard.Draws;
 
             // Assert
-            Assert.AreEqual(expectedDraws, actualDraws);
+            Assert.AreEqual(expected, actual);
         }
 
+        /// <summary>
+        /// 1 should be returned when an empty Scoreboard is created and 
+        /// Update() is invoked with argument P2WIN andd its property 
+        /// GamesPlayed is called
+        /// </summary>
         [TestMethod]
         public void Update_P2WIN_ReturnsScoreboardWithOneGamePlayed()
         {
             // Arrange
             Scoreboard actualScoreboard = CreateEmptyScoreboard();
             RPSP1P2Result newRPSresult = RPSP1P2Result.P2WIN;
-            int expectedGamesPlayed = 1;
+            int expected = 1;
 
             // Act
             actualScoreboard.Update(newRPSresult);
-            int actualGamesPlayed = actualScoreboard.GamesPlayed;
+            int actual = actualScoreboard.GamesPlayed;
 
             // Assert
-            Assert.AreEqual(expectedGamesPlayed, actualGamesPlayed);
+            Assert.AreEqual(expected, actual);
         }
 
+        /// <summary>
+        /// 0 should be returned when an empty Scoreboard is created and 
+        /// Update() is invoked with argument DRAW andd its property 
+        /// Player1Wins is called
+        /// </summary>
         [TestMethod]
         public void Update_DRAW_ReturnsScoreboardWithNoWinForPlayerOne()
         {
             // Arrange
             Scoreboard actualScoreboard = CreateEmptyScoreboard();
             RPSP1P2Result newRPSresult = RPSP1P2Result.DRAW;
-            int expectedP1Wins = 0;
+            int expected = 0;
 
             // Act
             actualScoreboard.Update(newRPSresult);
-            int actualP1Wins = actualScoreboard.Player1Wins;
+            int actual = actualScoreboard.Player1Wins;
 
             // Assert
-            Assert.AreEqual(expectedP1Wins, actualP1Wins);
+            Assert.AreEqual(expected, actual);
         }
 
+        /// <summary>
+        /// 0 should be returned when an empty Scoreboard is created and 
+        /// Update() is invoked with argument DRAW and its property 
+        /// Player2Wins is called
+        /// </summary>
         [TestMethod]
         public void Update_DRAW_ReturnsScoreboardWithNoWinForPlayerTwo()
         {
             // Arrange
             Scoreboard actualScoreboard = CreateEmptyScoreboard();
             RPSP1P2Result newRPSresult = RPSP1P2Result.DRAW;
-            int expectedP2Wins = 0;
+            int expected = 0;
 
             // Act
             actualScoreboard.Update(newRPSresult);
-            int actualP2Wins = actualScoreboard.Player2Wins;
+            int actual = actualScoreboard.Player2Wins;
 
             // Assert
-            Assert.AreEqual(expectedP2Wins, actualP2Wins);
+            Assert.AreEqual(expected, actual);
         }
 
+        /// <summary>
+        /// 1 should be returned when an empty Scoreboard is created and 
+        /// Update() is invoked with argument DRAW and its property 
+        /// Draws is called
+        /// </summary>
         [TestMethod]
         public void Update_DRAW_ReturnsScoreboardWithOneDraw()
         {
             // Arrange
             Scoreboard actualScoreboard = CreateEmptyScoreboard();
             RPSP1P2Result newRPSresult = RPSP1P2Result.DRAW;
-            int expectedDraws = 1;
+            int expected = 1;
 
             // Act
             actualScoreboard.Update(newRPSresult);
-            int actualDraws = actualScoreboard.Draws;
+            int actual = actualScoreboard.Draws;
 
             // Assert
-            Assert.AreEqual(expectedDraws, actualDraws);
+            Assert.AreEqual(expected, actual);
         }
 
+        /// <summary>
+        /// 1 should be returned when an empty Scoreboard is created and 
+        /// Update() is invoked with argument DRAW and its property 
+        /// GamesPlayed is called
+        /// </summary>
         [TestMethod]
         public void Update_DRAW_ReturnsScoreboardWithOneGamePlayed()
         {
             // Arrange
             Scoreboard actualScoreboard = CreateEmptyScoreboard();
             RPSP1P2Result newRPSresult = RPSP1P2Result.DRAW;
-            int expectedGamesPlayed = 1;
+            int expected = 1;
 
             // Act
             actualScoreboard.Update(newRPSresult);
-            int actualGamesPlayed = actualScoreboard.GamesPlayed;
+            int actual = actualScoreboard.GamesPlayed;
 
             // Assert
-            Assert.AreEqual(expectedGamesPlayed, actualGamesPlayed);
+            Assert.AreEqual(expected, actual);
         }
 
+        /// <summary>
+        /// 0 should be returned when an empty Scoreboard is created and 
+        /// Reset() is invoked and its property GamesPlayed is called
+        /// </summary>
         [TestMethod]
         public void Reset_EmptyScoreboard_ReturnsScoreboardWithNoGamePlayed()
         {
             // Arrange
             Scoreboard scoreboard = CreateEmptyScoreboard();
-            int expectedGamesPlayed = 0;
+            int expected = 0;
 
             // Act
             scoreboard.Reset();
+            int actual = scoreboard.GamesPlayed;
 
             // Assert
-            Assert.AreEqual(expectedGamesPlayed, scoreboard.GamesPlayed);
+            Assert.AreEqual(expected, actual);
         }
 
+        /// <summary>
+        /// 0 should be returned when an empty Scoreboard is created and 
+        /// Reset() is invoked and its property Draws is called
+        /// </summary>
         [TestMethod]
         public void Reset_EmptyScoreboard_ReturnsScoreboardWithNoDraw()
         {
             // Arrange
             Scoreboard scoreboard = CreateEmptyScoreboard();
-            int expectedDraws = 0;
+            int expected = 0;
 
             // Act
             scoreboard.Reset();
+            int actual = scoreboard.Draws;
 
             // Assert
-            Assert.AreEqual(expectedDraws, scoreboard.Draws);
+            Assert.AreEqual(expected, actual);
         }
 
+        /// <summary>
+        /// 0 should be returned when an empty Scoreboard is created and 
+        /// Reset() is invoked and its property Player1Wins is called
+        /// </summary>
         [TestMethod]
         public void Reset_EmptyScoreboard_ReturnsScoreboardWithNoWinForPlayerOne()
         {
             // Arrange
             Scoreboard scoreboard = CreateEmptyScoreboard();
-            int expectedP1Wins = 0;
+            int expected = 0;
 
             // Act
             scoreboard.Reset();
+            int actual = scoreboard.Player1Wins;
 
             // Assert
-            Assert.AreEqual(expectedP1Wins, scoreboard.Player1Wins);
+            Assert.AreEqual(expected, actual);
         }
 
+        /// <summary>
+        /// 0 should be returned when an empty Scoreboard is created and 
+        /// Reset() is invoked and its property Player2Wins is called
+        /// </summary>
         [TestMethod]
         public void Reset_EmptyScoreboard_ReturnsScoreboardWithNoWinForPlayerTwo()
         {
             // Arrange
             Scoreboard scoreboard = CreateEmptyScoreboard();
-            int expectedP2Wins = 0;
+            int expected = 0;
 
             // Act
             scoreboard.Reset();
+            int actual = scoreboard.Player2Wins;
 
             // Assert
-            Assert.AreEqual(expectedP2Wins, scoreboard.Player2Wins);
+            Assert.AreEqual(expected, actual);
         }
 
+        /// <summary>
+        /// 0 should be returned when a non empty Scoreboard is created and 
+        /// Reset() is invoked and its property GamesPlayed is called
+        /// </summary>
         [TestMethod]
         public void Reset_NonEmptyScoreboard_ReturnsScoreboardWithNoGamePlayed()
         {
             // Arrange
             Scoreboard scoreboard = CreateNonEmptyScoreboard();
-            int expectedGamesPlayed = 0;
+            int expected = 0;
 
             // Act
             scoreboard.Reset();
+            int actual = scoreboard.GamesPlayed;
 
             // Assert
-            Assert.AreEqual(expectedGamesPlayed, scoreboard.GamesPlayed);
+            Assert.AreEqual(expected, actual);
         }
 
+        /// <summary>
+        /// 0 should be returned when a non empty Scoreboard is created and 
+        /// Reset() is invoked and its property Draws is called
+        /// </summary>
         [TestMethod]
         public void Reset_NonEmptyScoreboard_ReturnsScoreboardWithNoDraw()
         {
             // Arrange
             Scoreboard scoreboard = CreateNonEmptyScoreboard();
-            int expectedDraws = 0;
+            int expected = 0;
 
             // Act
             scoreboard.Reset();
+            int actual = scoreboard.Draws;
 
             // Assert
-            Assert.AreEqual(expectedDraws, scoreboard.Draws);
+            Assert.AreEqual(expected, actual);
         }
 
+        /// <summary>
+        /// 0 should be returned when a non empty Scoreboard is created and 
+        /// Reset() is invoked and its property Player1Wins is called
+        /// </summary>
         [TestMethod]
         public void Reset_NonEmptyScoreboard_ReturnsScoreboardWithNoWinForPlayerOne()
         {
             // Arrange
             Scoreboard scoreboard = CreateNonEmptyScoreboard();
-            int expectedP1Wins = 0;
+            int expected = 0;
 
             // Act
             scoreboard.Reset();
+            int actual = scoreboard.Player1Wins;
 
             // Assert
-            Assert.AreEqual(expectedP1Wins, scoreboard.Player1Wins);
+            Assert.AreEqual(expected, actual);
         }
 
+        /// <summary>
+        /// 0 should be returned when a non empty Scoreboard is created and 
+        /// Reset() is invoked and its property Player2Wins is called
+        /// </summary>
         [TestMethod]
         public void Reset_NonEmptyScoreboard_ReturnsScoreboardWithNoWinForPlayerTwo()
         {
             // Arrange
             Scoreboard scoreboard = CreateNonEmptyScoreboard();
-            int expectedP2Wins = 0;
+            int expected = 0;
 
             // Act
             scoreboard.Reset();
+            int actual = scoreboard.Player2Wins;
 
             // Assert
-            Assert.AreEqual(expectedP2Wins, scoreboard.Player2Wins);
+            Assert.AreEqual(expected, actual);
         }
     }
 }
